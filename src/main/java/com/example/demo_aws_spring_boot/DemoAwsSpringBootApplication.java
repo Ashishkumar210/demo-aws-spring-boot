9e0297e43a79850fddb92e1ab2e1bd216704fc97
ashish <ashishmahanta121@gmail.com>
2025-09-08 00:58:18 +0530

    fi

---
+++ src/main/java/com/example/demo_aws_spring_boot/DemoAwsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_aws_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_aws_spring_boot/DemoAwsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_aws_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
